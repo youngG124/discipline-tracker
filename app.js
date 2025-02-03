97e930add39d55fe7cb5191ba4c655e4e35f6d85
newyoung124 <newyoung124@naver.com>
2025-02-04 00:24:11 +0900

    another app.get

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('쮜이이...');
+})
+
+app.get('/1', (req, res) => {
+    res.send('듀...');
 })
 
 app.listen(port, () => {
